924c28a6079d8ae4dd1287459821fc809b219e8b
zhh <zhh@163.com>
2017-04-19 16:45:24 +0800

    测试提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,7 +17,7 @@ Page({
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
-      //更新数据--------测试git提交代码
+      //更新数据
       that.setData({
         userInfo:userInfo
       })
